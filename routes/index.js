30b512484f0a320ddc2adaa11abcf40e6e88df57
arjunm13 <arjunmahen13@gmail.com>
2016-05-18 11:56:05 -0400

    update

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/index', function(req, res, next) {
+  res.render('page', { title: 'Express' });
+});
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('resume');
 });
 
 module.exports = router;
